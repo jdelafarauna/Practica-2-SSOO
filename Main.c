341a36522a753739a3b41667f294d0cc2c5463c6
José de la fuente <jdelafarauna@gmail.com>
2024-12-26 18:43:18 +0100

    Estructura a medias

--- Main.c
+++ Main.c
@@ -3,8 +3,27 @@
 //
 
 #include "stdio.h"
+#include <stdlib.h>
+
+const int MAXTandas = 10;
+
+typedef struct{
+    int vacunasDisp;
+}TCentro;
+
+typedef struct{
+    int numVacunas;
+}TEmpresa;
+
+
+void initEmpresas(){
+    int *Empresas = malloc (sizeof (int) * 3);
+}
 
 int main(){
-    printf("Prueba de ejecucion");
+    printf("Comienzo del programa");
+
+
+    printf("Fin del programa");
     return 0;
 }
