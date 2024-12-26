bc779c6007bb4e7bf2f710cf12dcaf77bee8861f
José de la fuente <jdelafarauna@gmail.com>
2024-12-26 19:53:46 +0100

    Comienzo

--- Main.c
+++ Main.c
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 
 const int MAXTandas = 10;
+const int MAXCentros = 5;
+
+int vacunasIni;
 
 typedef struct{
     int vacunasDisp;
@@ -15,15 +18,31 @@ typedef struct{
     int numVacunas;
 }TEmpresa;
 
+void leerFicheroEntrada();
+TCentro* initCentro();
+void initEmpresas();
 
-void initEmpresas(){
-    int *Empresas = malloc (sizeof (int) * 3);
-}
 
-int main(){
+int main(int argc, char *argv[]){
     printf("Comienzo del programa");
-
-
+    leerFicheroEntrada();
+    vacunasIni = 100;
+    int numCentros;
     printf("Fin del programa");
     return 0;
+}
+
+void leerFicheroEntrada(){
+
+}
+
+TCentro* initCentro(){
+    TCentro *centro = malloc(sizeof (TCentro) * 5);
+    for (int i = 0; i < MAXCentros; i++ ){
+        centro[i].vacunasDisp = vacunasIni;
+    }
+    return centro;
+}
+void initEmpresas(){
+    int *Empresas = malloc (sizeof (int) * 3);
 }
